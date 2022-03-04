f93716b689627e5f11c1fb79613f3e22c98def90
nuuuwan <nuuuwan@gmail.com>
2022-03-04 10:00:33 +0530

    Added get_count

--- _utils.py
+++ _utils.py
@@ -1,3 +1,11 @@
 from utils import logx
 
 log = logx.get_logger('books')
+
+def get_count(iter, func_get_keys):
+    key_to_count = {}
+    for item in iter:
+        keys = func_get_keys(item)
+        for key in keys:
+            key_to_count[key] = key_to_count.get(key, 0) + 1
+    return key_to_count
