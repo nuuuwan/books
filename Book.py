b88f70cf3cfe082c3a0d0a524f9c40ea9c728b28
nuuuwan <nuuuwan@gmail.com>
2022-03-04 10:38:34 +0530

    Added parse_clz_author

--- Book.py
+++ Book.py
@@ -4,6 +4,13 @@ DELIM_AUTHOR_LIST = '|'
 DELIM_AUTHOR_LIST_WITH_SPACES = ' | '
 
 
+def parse_clz_author(author):
+    return list(map(
+        lambda author: author.strip(),
+        author.split(DELIM_AUTHOR_LIST),
+    ))
+
+
 def normalize_dewey(raw_dewey):
     raw_dewey = raw_dewey.replace("/", "").strip()
     try:
@@ -53,10 +60,7 @@ class Book:
     def buildFromCLZDatum(d):
         return Book(
             d['Dewey'],
-            list(map(
-                lambda author: author.strip(),
-                d['Author'].split(DELIM_AUTHOR_LIST),
-            )),
+            parse_clz_author(d['Author']),
             d['Title'],
             d['ISBN'],
         )
